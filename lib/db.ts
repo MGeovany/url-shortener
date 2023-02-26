a61d1fc8a4a7b751070d8731b368bb8b77732469
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2023-02-25 23:23:59 -0600

    feat: :card_file_box: added new user table to db

---
+++ lib/db.ts
@@ -0,0 +1,89 @@
+import { PrismaClient } from "@prisma/client";
+
+interface ShortLink {
+  url: string;
+  shortUrl: string;
+  userId: number;
+}
+
+interface LinkData {
+  id?: number;
+  url?: string;
+  shortUrl?: string;
+  createdAt?: Date;
+  userId?: number | null;
+  User?: number;
+}
+// Link handlers
+export const createShortLink = async (
+  url: string,
+  shortUrl: string,
+  userId: number
+): Promise<LinkData> => {
+  const prisma = new PrismaClient();
+
+  const linkData = await prisma.link.create({
+    data: { url, shortUrl, userId } as ShortLink,
+  });
+
+  await prisma.user.update({
+    where: { id: userId },
+    data: {
+      links: {
+        connect: {
+          id: linkData.id,
+        },
+      },
+    },
+  });
+
+  await prisma.$disconnect();
+  console.log("im link data", linkData);
+  return linkData;
+};
+
+export const getURLbyShortLink = async (shortUrl: string) => {
+  const prisma = new PrismaClient();
+  const data = await prisma.link.findUnique({
+    where: { shortUrl },
+  });
+
+  if (!data) return null;
+
+  await prisma.$disconnect();
+  return data;
+};
+
+/* export const updateShortLinkClicks = async (shortUrl: string, data: any) => {
+  const prisma = new PrismaClient();
+  const response = await prisma.link.update({
+    where: { shortUrl },
+    data: { clicks: data.clicks + 1 },
+  });
+  await prisma.$disconnect();
+  return response;
+}; */
+
+// User handlers
+export const getUserByEmail = async (email: string) => {
+  const prisma = new PrismaClient();
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+    include: {
+      links: true,
+    },
+  });
+  await prisma.$disconnect();
+  return user;
+};
+
+export const createUser = async (email: string) => {
+  const prisma = new PrismaClient();
+  const user = await prisma.user.create({
+    data: { email },
+  });
+  await prisma.$disconnect();
+  return user;
+};
---
+++ prisma/migrations/20230225004410_added_user_table/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "Link" ADD COLUMN     "userId" INTEGER;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Link" ADD CONSTRAINT "Link_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
