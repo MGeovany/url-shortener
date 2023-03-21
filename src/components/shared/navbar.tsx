dde779785e336016c218d201cabcd43e9a740be4
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2023-03-20 23:15:47 -0600

    feat: added logo on the navbar

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -14,29 +14,37 @@ export function Navbar() {
     <div className="w-full">
       <div className={`fixed md:px-10 top-0 w-full z-30 transition-all`}>
         <div className="mx-5 flex h-16 max-w-screen-xl items-center md:justify-end xs:justify-end xl:mx-auto">
-          <ul className="flex flex-row font-semibold">
-            <li className="mx-5 flex items-center text-sm hover:underline">
-              {currentPath === "/" ? (
-                <Link href={"/dashboard"}>Dashboard</Link>
-              ) : (
-                <Link href={"/"}>Home</Link>
-              )}
+          <ul className="flex flex-row font-semibold w-full justify-between">
+            <li className="font-extrabold hover:underline text-xl">
+              <a href="/">
+                zoro.
+                <span className="text-green-400">cut</span>
+              </a>
             </li>
+            <div className="flex flex-row">
+              <li className="mx-5 flex items-center text-sm hover:underline  justify-end">
+                {currentPath === "/" ? (
+                  <Link href={"/dashboard"}>Dashboard</Link>
+                ) : (
+                  <Link href={"/"}>Home</Link>
+                )}
+              </li>
 
-            {!session && status !== "loading" ? (
-              <>
-                <li>
-                  <button
-                    className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
-                    onClick={() => setShowSignInModal(true)}
-                  >
-                    Sign In
-                  </button>
-                </li>
-              </>
-            ) : (
-              <UserDropdown />
-            )}
+              {!session && status !== "loading" ? (
+                <>
+                  <li>
+                    <button
+                      className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
+                      onClick={() => setShowSignInModal(true)}
+                    >
+                      Sign In
+                    </button>
+                  </li>
+                </>
+              ) : (
+                <UserDropdown />
+              )}
+            </div>
           </ul>
           <SignInModal />
         </div>
