cfbfaa5e1f1056ce26c3aa6699bf65373abff35c
mgeovany <marlongeo1999@gmail.com>
2024-03-24 22:25:13 -0600

    chore: removed slash

--- src/components/layouts/baseLayout.tsx
+++ src/components/layouts/baseLayout.tsx
@@ -21,11 +21,11 @@ const BaseLayout = ({
         <meta name="viewport" content="width=device-width" />
         <meta
           property="twitter:image"
-          content={`${BASE_URL_PRODUCTION}/twitter-preview.webp`}
+          content={`${BASE_URL_PRODUCTION}twitter-preview.webp`}
         />
         <meta
           property="twitter:card"
-          content={`${BASE_URL_PRODUCTION}/twitter-preview.webp`}
+          content={`${BASE_URL_PRODUCTION}twitter-preview.webp`}
         />
         <meta property="twitter:title" content={title} />
         <meta property="twitter:description" content={description} />
@@ -33,7 +33,7 @@ const BaseLayout = ({
         <meta property="description" content={description} />
         <meta
           property="og:image"
-          content={`${BASE_URL_PRODUCTION}/twitter-preview.webp`}
+          content={`${BASE_URL_PRODUCTION}twitter-preview.webp`}
         />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
