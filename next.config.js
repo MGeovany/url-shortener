fcb2b9a462cf90a36108d4210c0e41fc05d9a860
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2024-03-27 01:31:53 -0600

    feat: updated images logic

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-/** *
- * @type {import('next').NextConfig}
- */
-
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
-
-module.exports = nextConfig;
