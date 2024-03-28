c6a555d0ad594ca762d34db8917ac26f915b8561
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2024-03-27 22:22:28 -0600

    refactor: added docstring

--- src/pages/api/deleteLink.ts
+++ src/pages/api/deleteLink.ts
@@ -1,16 +1,27 @@
-import { deleteLink } from "lib/db";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { deleteLink } from "lib/db";
+import { getLinkIdFromRequest } from "@/utils/getLinkIdFromRequest";
 
+/**
+ * Request handler for DELETE requests.
+ * Deletes a link with the specified ID.
+ * @param req The request object containing information about the incoming HTTP request.
+ * @param res The response object used to send the HTTP response.
+ * @returns A 204 status code with no content if the deletion is successful.
+ *          A 500 status code with an "Internal Server Error" message if an error occurs.
+ */
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "DELETE") {
-    return res.status(405).json({ message: "Method Not Allowed" });
-  }
-  const linkId = Number(req.query.linkId);
   try {
+    if (req.method !== "DELETE") {
+      return res.status(405).json({ message: "Method Not Allowed" });
+    }
+
+    const linkId = getLinkIdFromRequest(req);
     await deleteLink(linkId);
+
     return res.status(204).end();
   } catch (error) {
     console.error(error);
